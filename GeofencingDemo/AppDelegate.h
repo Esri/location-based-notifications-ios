112251b2cfbd378195c0e4bc15834c571a3a9458
Aaron Parecki <aaron@parecki.com>
2016-03-01 14:15:26 -0800

    Initial Commit

---
+++ GeofencingDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GeofencingDemo
+//
+//  Created by Aaron Parecki on 3/1/16.
+//  Copyright © 2016 Aaron Parecki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GeofencingDemo/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  GeofencingDemo
+//
+//  Created by Aaron Parecki on 3/1/16.
+//  Copyright © 2016 Aaron Parecki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
